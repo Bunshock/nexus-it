883287b23d5f345d4ab3209347ef5f48d6684d3b
Bunshock <leomanto98@gmail.com>
2026-04-29 20:33:49 -0300

    App now opens at 95% of screen height

--- desktop-app/src/main/java/com/bunshock/note_app_for_it_frontend/App.java
+++ desktop-app/src/main/java/com/bunshock/note_app_for_it_frontend/App.java
@@ -2,16 +2,24 @@ package com.bunshock.note_app_for_it_frontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Siglo 21 - Generador de Notas IT");
+        Parent root = FXMLLoader.load(getClass().getResource("views/MainView.fxml"));
+        
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        double height = visualBounds.getHeight() * 0.95;
+        double width = 1275.0;
+        
+        Scene scene = new Scene(root, width, height);
         stage.setScene(scene);
+        stage.setTitle("Siglo 21 - Generador de Notas IT");
         stage.show();
     }
 
